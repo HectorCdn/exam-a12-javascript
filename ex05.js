8f6cccd169f2e3a3ed46ec7d7c0163f9b77474c7
HectorCdn <hcottachaudon@edenschool.fr>
2025-01-23 11:01:54 +0100

    dzqdqdd

---
+++ ex05.js
@@ -0,0 +1,10 @@
+function compterVoyelles(chaine) {
+  
+    if (compterVoyelles.include(a, e, i, o, u, y){
+        console.log("")
+        }
+  
+    return chaine;
+  }
+  
+  console.log(compterVoyelles("bonjour")); // Résultat attendu : 3
